db90fc6fce1fce89c915544013c14f7f73ac9fd3
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2020-07-05 12:24:38 -0400

    Use relativity property

--- org-relativity.el
+++ org-relativity.el
@@ -17,6 +17,19 @@ String is expected to be of form HH:MM."
         (+ (* (car units) 60) (cadr units)))
     0))
 
+;;@BUG: what happens when range spans over a day?
+;;do we want multiple timestamps? e.g.
+;;<t1>--<t2>?
+(defun org-relativity-ranged-timesstamp (timestamp effort)
+  "Calculate a time range of form HH:MM-HH:MM.
+Argument TIMESTAMP tbd.
+Argument EFFORT tbd.")
+
+(defun org-relativity-entry-p ()
+  "Return t if entry at point has a non-nil relativity property, nil otherwise."
+  (when-let ((relativity (org-entry-get (point) "relativity")))
+    (and (read relativity) t)))
+
 (defun org-relativity-schedule-tree (&rest _args)
   "Do something."
   (interactive)
@@ -24,46 +37,47 @@ String is expected to be of form HH:MM."
         scheduled
         effort)
     (save-excursion
-      (save-restriction
-        (while (org-up-heading-safe))
-        (org-narrow-to-subtree)
-        (org-map-tree (lambda ()
-                        (let ((p (point)))
-                          (when scheduled
-                            (org-schedule nil scheduled)
-                            (save-excursion
-                              (save-restriction
-                                (beginning-of-line)
-                                (org-narrow-to-subtree)
-                                (when (re-search-forward org-scheduled-time-regexp nil t)
-                                  (org-timestamp-change (org-relativity-effort-to-minutes effort) 'minute)))))
-                          (setq scheduled (org-element-interpret-data
-                                           (org-timestamp-from-time
-                                            (org-get-scheduled-time p) t))
-                                effort (org-entry-get p "EFFORT")))))))))
+      (while (org-up-heading-safe))
+      (when (org-relativity-entry-p)
+        (save-restriction
+          (org-narrow-to-subtree)
+          (org-map-tree (lambda ()
+                          (let ((p (point)))
+                            (when scheduled
+                              (org-schedule nil scheduled)
+                              (save-excursion
+                                (save-restriction
+                                  (org-narrow-to-subtree)
+                                  (when (re-search-forward org-scheduled-time-regexp nil t)
+                                    (org-timestamp-change (org-relativity-effort-to-minutes effort) 'minute)))))
+                            (setq scheduled (org-element-interpret-data
+                                             (org-timestamp-from-time
+                                              (org-get-scheduled-time p) t))
+                                  effort (org-entry-get p "EFFORT"))))))))))
+
 (progn
-  (advice-remove #'org-timestamp-up   #'org-relativity-advice)
-  (advice-remove #'org-timestamp-down #'org-relativity-advice)
-  (advice-remove #'org-set-effort     #'org-relativity-schedule-tree))
+  (advice-remove #'org-timestamp-up      #'org-relativity-numeric-move-advice)
+  (advice-remove #'org-timestamp-down    #'org-relativity-numeric-move-advice)
+  (advice-remove #'org-move-subtree-up   #'org-relativity-numeric-move-advice)
+  (advice-remove #'org-move-subtree-down #'org-relativity-numeric-move-advice)
+  (advice-remove #'org-set-effort        #'org-relativity-schedule-tree))
 
 ;;@RESEARCH: why can't we pass numeric arg with simple :after advice?
+;;@INCOMPLETE: what other commands should we advise?
 (progn
-  (advice-add #'org-timestamp-up   :around #'org-relativity-timestamp-advice)
-  (advice-add #'org-timestamp-down :around #'org-relativity-timestamp-advice)
-  (advice-add #'org-set-effort     :after #'org-relativity-schedule-tree))
+  (advice-add #'org-timestamp-up      :around #'org-relativity-numeric-move-advice)
+  (advice-add #'org-timestamp-down    :around #'org-relativity-numeric-move-advice)
+  (advice-add #'org-move-subtree-up   :around #'org-relativity-numeric-move-advice)
+  (advice-add #'org-move-subtree-down :around #'org-relativity-numeric-move-advice)
+  (advice-add #'org-set-effort        :after #'org-relativity-schedule-tree))
 
-(defun org-relativity-timestamp-advice (advised &optional arg)
-  "."
+(defun org-relativity-numeric-move-advice (advised &optional arg)
+  ".
+Argument ADVISED tbd.
+Optional argument ARG tbd."
+  (interactive "p")
   (funcall advised arg)
   (org-relativity-schedule-tree))
-
-
-
-
-;;advise org-timestamp-up/down?
-;;advise org-clock-modify-effort-estimate
-;;advise org-insert-property
-;;(org-timestamp-change)
 
 (provide 'org-relativity)
 
